8dea61b522a5c90ed6b18971fc38c014837b72d0
NilVinyals <31955883+NilVinyals@users.noreply.github.com>
2021-06-25 10:48:47 +0200

    Initial Commit with calculator example

---
+++ MyFirstAppTests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Tests
+{
+    [TestClass()] // Custom attributes
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Add(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Substract(0, 2) == -2);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Multiply(9, 9) == 81);
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Division(9, 3) == 3);
+        }
+    }
+}
