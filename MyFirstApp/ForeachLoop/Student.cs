de303b7a935eb2de312a55dc2194c6e81687cb6e
NilVinyals <31955883+NilVinyals@users.noreply.github.com>
2021-06-25 13:48:16 +0200

    Foreach implementation with ArrayList example. Closes #1

---
+++ MyFirstApp/ForeachLoop/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.ForeachLoop
+{
+    class Student
+    {
+        public int StudentId { get; set; } // genera ua variable privada per sota
+
+        public String Name { get; set; }  // cntrol k + cntrol d per tabular bé tot
+
+        public String Surname { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
