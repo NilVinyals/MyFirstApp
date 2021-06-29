4849ce58a95cc98a794ae5417c90b6d789cfbb31
NilVinyals <31955883+NilVinyals@users.noreply.github.com>
2021-06-29 12:37:21 +0200

    For loop implementation finished closes #4

--- MyFirstApp/ForLoop/frmForLoop.Designer.cs
+++ MyFirstApp/ForLoop/frmForLoop.Designer.cs
@@ -28,19 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(301, 122);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(199, 22);
+            this.txtStudentId.TabIndex = 9;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(301, 150);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(199, 22);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(301, 206);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(199, 22);
+            this.txtAge.TabIndex = 7;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(301, 178);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(199, 22);
+            this.txtSurname.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(346, 277);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 51);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // frmForLoop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.btnSave);
             this.Name = "frmForLoop";
             this.Text = "frmForLoop";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Button btnSave;
     }
 }
